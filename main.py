3237c5e7f4285e89016567d69117b178fa8ff198
NikolaiK25 <info.nikkin@gmail.com>
2025-12-14 17:11:04 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import ptbot
+from dotenv import load_dotenv
+import os
+import pytimeparse
+
+
+load_dotenv()
+
+TG_TOKEN = os.getenv('TG_TOKEN')
+TG_CHAT_ID = os.getenv('TG_CHAT_ID')
+
+
+def wait(chat_id, text):
+    delay_seconds = pytimeparse.parse(text)
+
+
+    if delay_seconds is None:
+        bot.send_message(chat_id,  "Не могу понять время. Укажите задержку, например: 5s, 30 секунд")
+
+    else:
+        message_id = bot.send_message(chat_id, f"Таймер запущен на {delay_seconds} секунд\n{render_progressbar(delay_seconds, 0)}")
+        
+        bot.create_countdown(delay_seconds, countdown,
+                            chat_id=chat_id,
+                            message_id=message_id,
+                            total=delay_seconds)
+        
+
+        bot.create_timer(delay_seconds, send_message, author_id=chat_id)
+
+
+def countdown(seconds_left, chat_id, message_id, total):
+    progress = total - seconds_left
+    bot.update_message(chat_id, message_id, 
+                        f"Осталось {seconds_left} секунд\n{render_progressbar(total, progress)}")
+
+
+def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
+    iteration = min(total, iteration)
+    percent = "{0:.1f}"
+    percent = percent.format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    pbar = fill * filled_length + zfill * (length - filled_length)
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+
+
+def send_message(author_id):
+    bot.send_message(author_id, "Время вышло")
+
+
+def main():
+    global bot
+    bot = ptbot.Bot(TG_TOKEN)
+    bot.reply_on_message(wait)
+    bot.run_bot()
+
+
+if __name__ == '__main__':
+    main()
